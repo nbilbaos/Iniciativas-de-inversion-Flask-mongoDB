1c41ba16b6cee57413688a27fb3cb786b461e960
nemesiobilbao <nemesio.bilbao@mayor.cl>
2025-04-28 13:24:05 -0400

    cambios en vista

--- application.py
+++ application.py
@@ -2,11 +2,11 @@ from app import create_app
 import traceback
 
 try:
-    app = create_app()
+    application = create_app()  # <<< Cambié 'app' por 'application'
 
     if __name__ == '__main__':
         print("Iniciando la aplicación Flask...")
-        app.run(host='0.0.0.0', port=5001)
+        application.run(host='0.0.0.0', port=5001)  # <<< También cambié aquí
 except Exception as e:
     print("Error al iniciar la aplicación:")
     print(traceback.format_exc())
