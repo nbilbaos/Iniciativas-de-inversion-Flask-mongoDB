3a98e6f004213a10fc495b2d5705c2687937a41a
nemesiobilbao <nemesio.bilbao@mayor.cl>
2025-04-29 09:01:10 -0400

    renombrado en run.py

--- run.py
+++ run.py
@@ -2,11 +2,12 @@ from app import create_app
 import traceback
 
 try:
-    application = create_app()  # <<< Cambié 'app' por 'application'
+    app = create_app()  # <<< Cambié 'app' por 'application'
 
     if __name__ == '__main__':
         print("Iniciando la aplicación Flask...")
-        application.run(host='0.0.0.0', port=5001)  # <<< También cambié aquí
+        app.run(host='0.0.0.0', port=5001)  # <<< También cambié aquí
 except Exception as e:
     print("Error al iniciar la aplicación:")
     print(traceback.format_exc())
+
