34f0971ac54daf84b7ea36dd1882adcb3df504f1
nemesiobilbao <nemesio.bilbao@mayor.cl>
2025-04-29 09:08:26 -0400

    renombrado en run.py

--- run.py
+++ run.py
@@ -2,12 +2,9 @@ from app import create_app
 import traceback
 
 try:
-    app = create_app()  # <<< Cambié 'app' por 'application'
-
-    if __name__ == '__main__':
-        print("Iniciando la aplicación Flask...")
-        app.run(host='0.0.0.0', port=5001)  # <<< También cambié aquí
+    app = create_app()  # Este objeto estará disponible para gunicorn
 except Exception as e:
     print("Error al iniciar la aplicación:")
     print(traceback.format_exc())
 
+# Nota: no uses app.run() aquí, Render + gunicorn se encargan
